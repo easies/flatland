8b57624c7bd29afecc9641c9bab0c64e8d61ad8d
Alex Lee <lee@ccs.neu.edu>
2011-02-21 18:50:33 -0500

    Add level teleporting.

--- flatland/generate_preload.sh
+++ flatland/generate_preload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for x in `ls data`; do
+for x in `ls data/*.png`; do
     echo "/* @pjs preload=\"data/$x\"; */"
 done
--- flatland/preprocessor/map.py
+++ flatland/preprocessor/map.py
@@ -4,25 +4,36 @@ import sys
 from PIL import Image
 
 
-def preprocess(out, im, name):
+def blah(out, im, color):
     width, height = im.size
-    out.write('var %s = function () { return this; };\n' % name)
-    out.write('%s.prototype.in_wall = function(x, y) {\n' % name)
     out.write('switch (x) {\n')
     for x in range(0, width):
         out.write('case %d:\n' % x)
         out.write('switch (y) {\n')
+        has_wall = False
         for y in range(0, height):
             p = im.getpixel((x, y))
-            if p == (0, 0, 0):
+            if p[:3] == color:
+                has_wall = True
                 out.write('case %d:\n' % y)
-        out.write('return true;\n')
+        if has_wall:
+            out.write('return true;\n')
         out.write('default:\n')
         out.write('return false;\n')
         out.write('}\n')
     out.write('default:\n')
     out.write('return false;\n')
     out.write('}\n')
+
+
+def preprocess(out, im, name):
+    out.write('var %s = function () { return this; };\n' % name)
+    out.write('%s.prototype.in_wall = function(x, y) {\n' % name)
+    blah(out, im, (0, 0, 0))
+    out.write('};\n')
+    # end
+    out.write('%s.prototype.reached_end = function(x, y) {\n' % name)
+    blah(out, im, (0x86, 0x83, 0xFF))
     out.write('};\n')
 
 
@@ -37,8 +48,9 @@ def main():
         name = name[0].upper() + name[1:]
         out = sys.stdout
         preprocess(out, im, name)
-    except:
+    except Exception as e:
         sys.stderr.write('Error loading file: %s\n' % filename)
+        sys.stderr.write('%s\n' % str(e))
         sys.exit(2)
 
 
